cacfcec3163e097824140264c5c1cc36716b605e
Viviane Perrotti <viperrotti@gmail.com>
2023-11-06 12:19:52 -0300

    uso da API Agenda

--- script.js
+++ script.js
@@ -2,6 +2,56 @@ var button = document.querySelector('.lista')
 var popup = document.querySelector('.popup-wrapper')
 var agenda = new Array()
 
+function fazGet(url){
+    let request = new XMLHttpRequest()
+    request.open("GET",url,false)
+    request.send()
+    return request.responseText
+}
+
+function criaCard(professor){
+    let card = document.createElement("div");
+    card.classList.add('card')
+
+    let prof = document.createElement("div");
+    prof.classList.add('prof')
+
+    let imagem = document.createElement("img");
+    imagem.setAttribute('src',professor.url)
+
+    let nomeProf = document.createElement("p");
+    nomeProf.innerHTML = professor.nome + ', professor(a) de ' + professor.materia
+
+    prof.appendChild(imagem);
+    prof.appendChild(nomeProf);
+
+    let button = document.createElement("button");
+    button.setAttribute('id', professor.id)
+    button.innerHTML = 'Agendar Aula'
+
+    let botao = document.createElement("div");
+    botao.classList.add('botao')
+    let link = document.createElement("a");
+    link.setAttribute('href', "")
+    botao.innerHTML = 'Conversar'
+
+    card.appendChild(prof);
+    card.appendChild(button);
+    card.appendChild(botao);
+
+    return card;
+}
+
+function main(){
+    let data = fazGet("http://vps44051.publiccloud.com.br:5000/api/Professores")
+    let professores = JSON.parse(data);
+    let tabela = document.getElementById("professores")
+    professores.forEach(element => {
+        let linha = criaCard(element);
+        tabela.appendChild(linha);
+    });
+}
+
 button.addEventListener('click', (event) => {
     const isButton = event.target.nodeName === 'BUTTON';
     if (isButton) {
@@ -38,5 +88,5 @@ function Enviar() {
 } 
 
 
-
+main()
 
